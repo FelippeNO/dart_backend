4a8314a464ad5ace6b51b09ebf274fc890092042
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-10-08 18:17:55 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# dart_backend
- 
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:postgres/postgres.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_router/shelf_router.dart';
+import 'dart:convert';
+
+void main() async {
+  await DataBaseClient.initializeDatabase();
+
+  final Router app = Router();
+  app.get('/get_user_name_by_id/<user_id>', AccountServices.handleGetUserNameById);
+  app.get('/get_user_age_by_id/<user_id>', AccountServices.handleGetUserAgeById);
+  app.post('/create_new_user', AccountServices.handleCreateNewUser);
+
+  final server = await shelf_io.serve(app, 'localhost', 8080);
+
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+class AccountServices {
+  static Future<Response> handleGetUserNameById(Request request) async {
+    final userId = request.params['user_id'];
+    String userName = await AccountRepository.getUserNameById(userId!);
+    Map<String, String> json = {"name": userName};
+    final encodedBody = jsonEncode(json);
+    return Response(200, body: encodedBody);
+  }
+
+  static Future<Response> handleGetUserAgeById(Request request) async {
+    final userId = request.params['user_id'];
+    int idade = await AccountRepository.getUserAgeById(userId!);
+    Map<String, int> json = {"idade": idade};
+    final encodedBody = jsonEncode(json);
+    return Response(200, body: encodedBody);
+  }
+
+  static Future<Response> handleCreateNewUser(Request request) async {
+    final String query = await request.readAsString();
+    final decodedBody = jsonDecode(query);
+
+    String nome = decodedBody["nome"];
+    int idade = int.parse(decodedBody["idade"]);
+    String email = decodedBody["email"];
+
+    bool wasUserCreated = await AccountRepository.createUser(
+      name: nome,
+      age: idade,
+      email: email,
+    );
+    return Response(200, body: wasUserCreated.toString());
+  }
+}
+
+class AccountRepository {
+  static Future<String> getUserNameById(String userId) async {
+    final int userIdInt = int.parse(userId);
+    List<List<dynamic>> results = await DataBaseClient.connection
+        .query("SELECT nome FROM pessoas WHERE user_id = @user_id", substitutionValues: {"user_id": userIdInt});
+    return results[0][0];
+  }
+
+  static Future<int> getUserAgeById(String userId) async {
+    final int userIdInt = int.parse(userId);
+    List<List<dynamic>> results = await DataBaseClient.connection
+        .query("SELECT idade FROM pessoas WHERE user_id = @user_id", substitutionValues: {"user_id": userIdInt});
+    return results[0][0];
+  }
+
+  static Future<bool> createUser({required String name, required int age, required String email}) async {
+    final query = await DataBaseClient.connection
+        .query("INSERT INTO pessoas (nome, idade, email) VALUES (@name, @idade, @email)", substitutionValues: {
+      "name": name,
+      "idade": age,
+      "email": email,
+    });
+    return true;
+  }
+}
+
+class DataBaseClient {
+  static PostgreSQLConnection get connection => _connection;
+
+  static final PostgreSQLConnection _connection =
+      PostgreSQLConnection("localhost", 5432, "dart", username: "postgres", password: "postgres");
+
+  static Future initializeDatabase() async => await _connection.open();
+}
