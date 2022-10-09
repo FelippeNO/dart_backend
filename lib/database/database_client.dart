305cd22a58f59a279ccb673aa3ec1b6e110f37ba
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-10-09 12:10:08 -0300

    Update database_client.dart

--- lib/database/database_client.dart
+++ lib/database/database_client.dart
@@ -1,10 +1,10 @@
 import 'package:postgres/postgres.dart';
 
 class DataBaseClient {
-  static const String _host = "dart.cvt4v5njnonp.us-east-1.rds.amazonaws.com";
-  static const String _database = "dart_backend";
-  static const String _username = "postgres";
-  static const String _password = "12345678";
+  static const String _host = String.fromEnvironment('HOST');
+  static const String _database = String.fromEnvironment('DATABASE');
+  static const String _username = String.fromEnvironment('USERNAME');
+  static const String _password = String.fromEnvironment('PASSWORD');
   static const int _port = 5432;
 
   static PostgreSQLConnection get connection => _connection;
