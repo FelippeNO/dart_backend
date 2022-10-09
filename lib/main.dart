e2a9a76b93d23fd5c72f9731a57693e1ad3be401
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-10-09 17:13:42 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:postgres/postgres.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart';
@@ -57,77 +56,6 @@ class NumbersRepository {
   }
 }
 
-class AccountServices {
-  static Future<Response> aaaa(Request request) async {
-    final query = await DataBaseClient.connection.query(
-        "INSERT INTO users (username, email, password) VALUES (@username, @email, @password)",
-        substitutionValues: {
-          "username": "123",
-          "email": "54342",
-          "password": "password2222",
-        });
-    return Response(200, body: "true");
-  }
-
-  static Future<Response> handleGetUserNameById(Request request) async {
-    final userId = request.params['user_id'];
-    String userName = await AccountRepository.getUserNameById(userId!);
-    Map<String, String> json = {"name": userName};
-    final encodedBody = jsonEncode(json);
-    return Response(200, body: encodedBody);
-  }
-
-  static Future<Response> handleGetUserAgeById(Request request) async {
-    final userId = request.params['user_id'];
-    int idade = await AccountRepository.getUserAgeById(userId!);
-    Map<String, int> json = {"idade": idade};
-    final encodedBody = jsonEncode(json);
-    return Response(200, body: encodedBody);
-  }
-
-  static Future<Response> handleCreateNewUser(Request request) async {
-    final String query = await request.readAsString();
-    final decodedBody = jsonDecode(query);
-
-    String nome = decodedBody["nome"];
-    int idade = int.parse(decodedBody["idade"]);
-    String email = decodedBody["email"];
-
-    bool wasUserCreated = await AccountRepository.createUser(
-      name: nome,
-      age: idade,
-      email: email,
-    );
-    return Response(200, body: wasUserCreated.toString());
-  }
-}
-
-class AccountRepository {
-  static Future<String> getUserNameById(String userId) async {
-    final int userIdInt = int.parse(userId);
-    List<List<dynamic>> results = await DataBaseClient.connection
-        .query("SELECT nome FROM pessoas WHERE user_id = @user_id", substitutionValues: {"user_id": userIdInt});
-    return results[0][0];
-  }
-
-  static Future<int> getUserAgeById(String userId) async {
-    final int userIdInt = int.parse(userId);
-    List<List<dynamic>> results = await DataBaseClient.connection
-        .query("SELECT idade FROM pessoas WHERE user_id = @user_id", substitutionValues: {"user_id": userIdInt});
-    return results[0][0];
-  }
-
-  static Future<bool> createUser({required String name, required int age, required String email}) async {
-    final query = await DataBaseClient.connection
-        .query("INSERT INTO pessoas (nome, idade, email) VALUES (@name, @idade, @email)", substitutionValues: {
-      "name": name,
-      "idade": age,
-      "email": email,
-    });
-    return true;
-  }
-}
-
 class NumbersTable {
   static const String tablename = "numbers";
   static const String id = "id";
@@ -149,3 +77,75 @@ class NumbersEntity {
     required this.multiply,
   });
 }
+
+
+// class AccountServices {
+//   static Future<Response> aaaa(Request request) async {
+//     final query = await DataBaseClient.connection.query(
+//         "INSERT INTO users (username, email, password) VALUES (@username, @email, @password)",
+//         substitutionValues: {
+//           "username": "123",
+//           "email": "54342",
+//           "password": "password2222",
+//         });
+//     return Response(200, body: "true");
+//   }
+
+//   static Future<Response> handleGetUserNameById(Request request) async {
+//     final userId = request.params['user_id'];
+//     String userName = await AccountRepository.getUserNameById(userId!);
+//     Map<String, String> json = {"name": userName};
+//     final encodedBody = jsonEncode(json);
+//     return Response(200, body: encodedBody);
+//   }
+
+//   static Future<Response> handleGetUserAgeById(Request request) async {
+//     final userId = request.params['user_id'];
+//     int idade = await AccountRepository.getUserAgeById(userId!);
+//     Map<String, int> json = {"idade": idade};
+//     final encodedBody = jsonEncode(json);
+//     return Response(200, body: encodedBody);
+//   }
+
+//   static Future<Response> handleCreateNewUser(Request request) async {
+//     final String query = await request.readAsString();
+//     final decodedBody = jsonDecode(query);
+
+//     String nome = decodedBody["nome"];
+//     int idade = int.parse(decodedBody["idade"]);
+//     String email = decodedBody["email"];
+
+//     bool wasUserCreated = await AccountRepository.createUser(
+//       name: nome,
+//       age: idade,
+//       email: email,
+//     );
+//     return Response(200, body: wasUserCreated.toString());
+//   }
+// }
+
+// class AccountRepository {
+//   static Future<String> getUserNameById(String userId) async {
+//     final int userIdInt = int.parse(userId);
+//     List<List<dynamic>> results = await DataBaseClient.connection
+//         .query("SELECT nome FROM pessoas WHERE user_id = @user_id", substitutionValues: {"user_id": userIdInt});
+//     return results[0][0];
+//   }
+
+//   static Future<int> getUserAgeById(String userId) async {
+//     final int userIdInt = int.parse(userId);
+//     List<List<dynamic>> results = await DataBaseClient.connection
+//         .query("SELECT idade FROM pessoas WHERE user_id = @user_id", substitutionValues: {"user_id": userIdInt});
+//     return results[0][0];
+//   }
+
+//   static Future<bool> createUser({required String name, required int age, required String email}) async {
+//     final query = await DataBaseClient.connection
+//         .query("INSERT INTO pessoas (nome, idade, email) VALUES (@name, @idade, @email)", substitutionValues: {
+//       "name": name,
+//       "idade": age,
+//       "email": email,
+//     });
+//     return true;
+//   }
+// }
